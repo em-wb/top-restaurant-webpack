dd9aeb8bd703cffa42a69f5ddbd78a39cdcabdd9
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-06 10:47:41 +0100

    Create contact.js

---
+++ src/contact.js
@@ -0,0 +1,23 @@
+export default function contact() {
+  const hero = document.querySelector(".hero");
+  const contactDiv = document.createElement("div");
+  contactDiv.classList.add("contactDiv");
+  hero.append(contactDiv);
+
+  const iconDiv = document.createElement("div");
+  iconDiv.classList.add("iconDiv");
+  contactDiv.append(iconDiv);
+
+  const iconClasses = ["fa-facebook", "fa-instagram", "fa-whatsapp"];
+  iconClasses.forEach((iconClass) => {
+    const icon = document.createElement("i");
+    icon.classList.add("fa-brands");
+    icon.classList.add(iconClass);
+    iconDiv.append(icon);
+  });
+
+  const address = document.createElement("p");
+  address.innerHTML = `<strong>Bertie's Bistro</strong><br/>
+   245 Carrot Lane <br/> London <br/><br/><strong>Open</strong><br/> 12.00-22.30 <br/>Tues-Sun`;
+  contactDiv.append(address);
+}
