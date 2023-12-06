c219e85b219dba60a1bbd45c15499b494137e8cf
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-06 13:56:53 +0100

    Improve code concision

--- src/index.js
+++ src/index.js
@@ -9,29 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const hero = document.querySelector(".hero");
   home(hero);
 
-  document.getElementById("navMenu").addEventListener("click", () => {
-    const hero = document.querySelector(".hero");
+  function handleNavLinkClick(moduleFunction) {
     const heroChild = document.querySelector(".heroChild");
     if (hero) {
       hero.removeChild(heroChild);
-      menu(hero);
+      moduleFunction(hero);
     }
+  }
+
+  document.getElementById("navMenu").addEventListener("click", () => {
+    handleNavLinkClick(menu);
   });
 
   document.getElementById("navHome").addEventListener("click", () => {
-    const heroChild = document.querySelector(".heroChild");
-    if (hero) {
-      hero.removeChild(heroChild);
-      home(hero);
-    }
+    handleNavLinkClick(home);
   });
 
   document.getElementById("navContact").addEventListener("click", () => {
-    const hero = document.querySelector(".hero");
-    const heroChild = document.querySelector(".heroChild");
-    if (hero) {
-      hero.removeChild(heroChild);
-      contact(hero);
-    }
+    handleNavLinkClick(contact);
   });
 });
