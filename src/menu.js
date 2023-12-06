8529f23494693abf344489336950ad53800d2149
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-06 15:44:06 +0100

    Update dom elements for menu module

--- src/menu.js
+++ src/menu.js
@@ -8,46 +8,115 @@ export default function menu(hero) {
   menuH1.textContent = "MENU";
   menuDiv.append(menuH1);
 
-  const menuInfo = document.createElement("p");
-  menuInfo.classList.add("menuInfo");
-  menuInfo.innerHTML = `<strong>STARTERS</strong> <br/>
-<strong>Avocado and Tomato Bruschetta</strong> <br/>
-Fresh avocado and diced tomatoes on toasted sourdough, drizzled with balsamic glaze.
-<br/>
-<strong>Quinoa Salad Rolls</strong> <br/>
-Quinoa, avocado, cucumber, and carrots wrapped in rice paper served with a sesame dipping sauce.
-<br/>
-<strong>Sweet Potato Fries</strong> <br/>
-Crispy sweet potato fries served with a chipotle aioli for dipping.
-<br/><br/>
-<strong>MAINS</strong><br/>
-<strong>Grilled Salmon with Mango Salsa</strong> <br/>
-Pan-seared salmon fillet topped with a refreshing mango salsa, served with quinoa.
-<br/>
-<strong>Truffle Mushroom Risotto</strong> <br/>
-Creamy risotto with wild mushrooms, truffle oil, and Parmesan cheese.
-<br/>
-<strong>Crispy Tofu Stir-Fry</strong> <br/>
-Crispy tofu cubes stir-fried with colorful vegetables in a sesame soy glaze, served with brown rice.
-<br/><br/>
-<strong>DESSERTS</strong><br/>
-<strong>Matcha Green Tea Cheesecake</strong> <br/>
-Creamy matcha-flavored cheesecake on a chocolate almond crust.
-<br/>
-<strong>Mango Coconut Panna Cotta</strong> <br/>
-Smooth and velvety coconut panna cotta topped with fresh mango compote.
-<br/>
-<strong>Dark Chocolate Mousse</strong> <br/>
-Rich and indulgent dark chocolate mousse topped with a dollop of whipped cream.
-<br/><br/>
-<strong>DRINKS</strong><br/>
-<strong>Classic Cappuccino</strong> <br/>
-Espresso with equal parts of steamed milk and foam.
-<br/>
-<strong>Blueberry Mint Iced Tea</strong> <br/>
-Iced tea infused with blueberries and fresh mint leaves.
-<br/>
-<strong>Orange Ginger Sparkler</strong> <br/>
-Sparkling water with a splash of fresh orange juice and a hint of ginger.`;
-  menuDiv.append(menuInfo);
+  function createAndAppend(tag, text, parent) {
+    const element = document.createElement(tag);
+    if (text) {
+      element.innerHTML = text;
+    }
+    parent.appendChild(element);
+    return element;
+  }
+
+  const menuSections = ["STARTERS", "MAINS", "DESSERTS", "DRINKS"];
+  menuSections.forEach((section) => {
+    createAndAppend("strong", section, menuDiv);
+    createAndAppend("br", null, menuDiv);
+
+    switch (section) {
+      case "STARTERS":
+        createAndAppend("strong", "Avocado and Tomato Bruschetta", menuDiv);
+        createAndAppend(
+          "p",
+          "Fresh avocado and diced tomatoes on toasted sourdough, drizzled with balsamic glaze.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Quinoa Salad Rolls", menuDiv);
+        createAndAppend(
+          "p",
+          "Quinoa, avocado, cucumber, and carrots wrapped in rice paper served with a sesame dipping sauce.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Sweet Potato Fries", menuDiv);
+        createAndAppend(
+          "p",
+          "Crispy sweet potato fries served with a chipotle aioli for dipping.",
+          menuDiv
+        );
+        break;
+
+      case "MAINS":
+        createAndAppend("strong", "Grilled Salmon with Mango Salsa", menuDiv);
+        createAndAppend(
+          "p",
+          "Pan-seared salmon fillet topped with a refreshing mango salsa, served with quinoa.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Truffle Mushroom Risotto", menuDiv);
+        createAndAppend(
+          "p",
+          "Creamy risotto with wild mushrooms, truffle oil, and Parmesan cheese.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Crispy Tofu Stir-Fry", menuDiv);
+        createAndAppend(
+          "p",
+          "Crispy tofu cubes stir-fried with colorful vegetables in a sesame soy glaze, served with brown rice.",
+          menuDiv
+        );
+        break;
+
+      case "DESSERTS":
+        createAndAppend("strong", "Matcha Green Tea Cheesecake", menuDiv);
+        createAndAppend(
+          "p",
+          "Creamy matcha-flavored cheesecake on a chocolate almond crust.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Mango Coconut Panna Cotta", menuDiv);
+        createAndAppend(
+          "p",
+          "Smooth and velvety coconut panna cotta topped with fresh mango compote.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Dark Chocolate Mousse", menuDiv);
+        createAndAppend(
+          "p",
+          "Rich and indulgent dark chocolate mousse topped with a dollop of whipped cream.",
+          menuDiv
+        );
+        break;
+
+      case "DRINKS":
+        createAndAppend("strong", "Classic Cappuccino", menuDiv);
+        createAndAppend(
+          "p",
+          "Espresso with equal parts of steamed milk and foam.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Blueberry Mint Iced Tea", menuDiv);
+        createAndAppend(
+          "p",
+          "Iced tea infused with blueberries and fresh mint leaves.",
+          menuDiv
+        );
+        createAndAppend("br", null, menuDiv);
+        createAndAppend("strong", "Orange Ginger Sparkler", menuDiv);
+        createAndAppend(
+          "p",
+          "Sparkling water with a splash of fresh orange juice and a hint of ginger.",
+          menuDiv
+        );
+        break;
+
+      default:
+        break;
+    }
+  });
 }
