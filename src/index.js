f2da16df71dd23d2c198b0d169de786e01d57fb0
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-06 10:48:48 +0100

    Update index.js with new modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import pageLoad from "./pageLoad";
+import home from "./home";
+import menu from "./menu";
+import contact from "./contact";
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   pageLoad();
+  // home();
+  // menu();
+  contact();
 });
