32814ef89cb5889d26d6846145cb6a0ddb94a584
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-08 12:16:27 +0100

    Change menu build to use constructor class and improve maintainability/readability

--- src/menu.js
+++ src/menu.js
@@ -8,115 +8,129 @@ export default function menu(hero) {
   menuH1.textContent = "MENU";
   menuDiv.append(menuH1);
 
-  function createAndAppend(tag, text, parent) {
-    const element = document.createElement(tag);
-    if (text) {
-      element.innerHTML = text;
+  class MenuBuilder {
+    constructor(parentElement) {
+      this.parentElement = parentElement;
     }
-    parent.appendChild(element);
-    return element;
+
+    addLineBreak() {
+      const br = document.createElement("br");
+      this.parentElement.appendChild(br);
+    }
+
+    addHeading(text) {
+      const heading = document.createElement("strong");
+      heading.textContent = text;
+      this.parentElement.appendChild(heading);
+      this.addLineBreak();
+    }
+
+    addParagraph(text) {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = text;
+      this.parentElement.appendChild(paragraph);
+      this.addLineBreak();
+    }
+
+    buildMenuSection(section, dishes) {
+      this.addHeading(section);
+
+      dishes.forEach((dish) => {
+        this.addHeading(dish.name);
+        this.addParagraph(dish.description);
+      });
+    }
+
+    buildMenu() {
+      const menuSections = [
+        {
+          section: "STARTERS",
+          dishes: [
+            {
+              name: "Avocado and Tomato Bruschetta",
+              description:
+                "Fresh avocado and diced tomatoes on toasted sourdough, drizzled with balsamic glaze.",
+            },
+            {
+              name: "Quinoa Salad Rolls",
+              description:
+                "Quinoa, avocado, cucumber, and carrots wrapped in rice paper served with a sesame dipping sauce.",
+            },
+            {
+              name: "Sweet Potato Fries",
+              description:
+                "Crispy sweet potato fries served with a chipotle aioli for dipping.",
+            },
+          ],
+        },
+        {
+          section: "MAINS",
+          dishes: [
+            {
+              name: "Grilled Salmon with Mango Salsa",
+              description:
+                "Pan-seared salmon fillet topped with a refreshing mango salsa, served with quinoa.",
+            },
+            {
+              name: "Truffle Mushroom Risotto",
+              description:
+                "Creamy risotto with wild mushrooms, truffle oil, and Parmesan cheese.",
+            },
+            {
+              name: "Crispy Tofu Stir-Fry",
+              description:
+                "Crispy tofu cubes stir-fried with colorful vegetables in a sesame soy glaze, served with brown rice.",
+            },
+          ],
+        },
+        {
+          section: "DESSERTS",
+          dishes: [
+            {
+              name: "Matcha Green Tea Cheesecake",
+              description:
+                "Creamy matcha-flavored cheesecake on a chocolate almond crust.",
+            },
+            {
+              name: "Mango Coconut Panna Cotta",
+              description:
+                "Smooth and velvety coconut panna cotta topped with fresh mango compote.",
+            },
+            {
+              name: "Dark Chocolate Mousse",
+              description:
+                "Rich and indulgent dark chocolate mousse topped with a dollop of whipped cream.",
+            },
+          ],
+        },
+        {
+          section: "DRINKS",
+          dishes: [
+            {
+              name: "Classic Cappuccino",
+              description:
+                "Espresso with equal parts of steamed milk and foam.",
+            },
+            {
+              name: "Blueberry Mint Iced Tea",
+              description:
+                "Iced tea infused with blueberries and fresh mint leaves.",
+            },
+            {
+              name: "Orange Ginger Sparkler",
+              description:
+                "Sparkling water with a splash of fresh orange juice and a hint of ginger.",
+            },
+          ],
+        },
+      ];
+
+      menuSections.forEach((menuSection) =>
+        this.buildMenuSection(menuSection.section, menuSection.dishes)
+      );
+    }
   }
 
-  const menuSections = ["STARTERS", "MAINS", "DESSERTS", "DRINKS"];
-  menuSections.forEach((section) => {
-    createAndAppend("strong", section, menuDiv);
-    createAndAppend("br", null, menuDiv);
-
-    switch (section) {
-      case "STARTERS":
-        createAndAppend("strong", "Avocado and Tomato Bruschetta", menuDiv);
-        createAndAppend(
-          "p",
-          "Fresh avocado and diced tomatoes on toasted sourdough, drizzled with balsamic glaze.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Quinoa Salad Rolls", menuDiv);
-        createAndAppend(
-          "p",
-          "Quinoa, avocado, cucumber, and carrots wrapped in rice paper served with a sesame dipping sauce.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Sweet Potato Fries", menuDiv);
-        createAndAppend(
-          "p",
-          "Crispy sweet potato fries served with a chipotle aioli for dipping.",
-          menuDiv
-        );
-        break;
-
-      case "MAINS":
-        createAndAppend("strong", "Grilled Salmon with Mango Salsa", menuDiv);
-        createAndAppend(
-          "p",
-          "Pan-seared salmon fillet topped with a refreshing mango salsa, served with quinoa.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Truffle Mushroom Risotto", menuDiv);
-        createAndAppend(
-          "p",
-          "Creamy risotto with wild mushrooms, truffle oil, and Parmesan cheese.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Crispy Tofu Stir-Fry", menuDiv);
-        createAndAppend(
-          "p",
-          "Crispy tofu cubes stir-fried with colorful vegetables in a sesame soy glaze, served with brown rice.",
-          menuDiv
-        );
-        break;
-
-      case "DESSERTS":
-        createAndAppend("strong", "Matcha Green Tea Cheesecake", menuDiv);
-        createAndAppend(
-          "p",
-          "Creamy matcha-flavored cheesecake on a chocolate almond crust.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Mango Coconut Panna Cotta", menuDiv);
-        createAndAppend(
-          "p",
-          "Smooth and velvety coconut panna cotta topped with fresh mango compote.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Dark Chocolate Mousse", menuDiv);
-        createAndAppend(
-          "p",
-          "Rich and indulgent dark chocolate mousse topped with a dollop of whipped cream.",
-          menuDiv
-        );
-        break;
-
-      case "DRINKS":
-        createAndAppend("strong", "Classic Cappuccino", menuDiv);
-        createAndAppend(
-          "p",
-          "Espresso with equal parts of steamed milk and foam.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Blueberry Mint Iced Tea", menuDiv);
-        createAndAppend(
-          "p",
-          "Iced tea infused with blueberries and fresh mint leaves.",
-          menuDiv
-        );
-        createAndAppend("br", null, menuDiv);
-        createAndAppend("strong", "Orange Ginger Sparkler", menuDiv);
-        createAndAppend(
-          "p",
-          "Sparkling water with a splash of fresh orange juice and a hint of ginger.",
-          menuDiv
-        );
-        break;
-
-      default:
-        break;
-    }
-  });
+  const menuBuilder = new MenuBuilder(menuDiv);
+  menuBuilder.buildMenu();
 }
